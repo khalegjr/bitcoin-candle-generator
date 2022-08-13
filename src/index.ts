7f47897a6079afccd28dc0e753ecca1b5b9eb258
Khaled <khalegjr@gmail.com>
2022-08-13 04:06:49 -0300

    primeira chamada da api

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import axios from "axios";
+
+config();
+
+const readMarktPrice = async (): Promise<number> => {
+  const result = await axios.get(process.env.PRICES_API);
+  const data = result.data;
+  const price = data.bitcoin.usd;
+  console.log(price);
+
+  return price;
+};
+
+readMarktPrice();
